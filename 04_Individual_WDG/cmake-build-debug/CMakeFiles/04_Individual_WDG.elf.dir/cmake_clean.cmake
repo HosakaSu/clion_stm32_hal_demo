210026ce1738a278064d60405dd8eaad1f81e3c2
LiuChangjie <liuchangjie@jari.cn>
2020-12-03 11:14:25 +0800

    添加独立看门狗项目

---
+++ 04_Individual_WDG/cmake-build-debug/CMakeFiles/04_Individual_WDG.elf.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "04_Individual_WDG.elf"
+  "04_Individual_WDG.elf.manifest"
+  "04_Individual_WDG.elf.pdb"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Core/Src/iwdg.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_iwdg.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/04_Individual_WDG.elf.dir/startup/startup_stm32f103xe.s.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/04_Individual_WDG.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 1. cmake project is not loaded 解决办法：
 
    新建clion 项目而不是直接拷贝，自动生成cmake CmakeLists.txt
-
+   03_USARTX 重定向fputc 函数使用 微雪课堂教程 https://www.waveshare.net/study/article-644-1.html
    
 
